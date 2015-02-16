135cb85cbf84b94ad2dc999b99dbcf1c6a22fc6e
Raymond Hoagland <hoaglan2@illinois.edu>
2015-02-16 16:37:03 -0600

    Cleanup
    minor cleanup in install script

--- node_install.sh
+++ node_install.sh
@@ -4,13 +4,10 @@ sudo apt-get install -y curl
 echo 'Get right version of nodejs, npm'
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get install -y nodejs
-
 sudo apt-get install -y npm
 
-echo 'Setting registry'
+echo 'Setting registry for npm'
 npm config set registry http://registry.npmjs.org/
-
-npm -g install npm@2.0.0
 
 echo 'Installing express'
 npm install -g express 

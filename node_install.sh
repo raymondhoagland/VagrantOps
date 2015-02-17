55d2e6075a4ad3426e5d014ef644061fa1f77a6c
Raymond Hoagland <hoaglan2@illinois.edu>
2015-02-17 00:27:53 -0600

    added pip

--- node_install.sh
+++ node_install.sh
@@ -1,12 +1,16 @@
 echo 'Installing curl'
 sudo apt-get install -y curl
 
+echo 'Installing pip'
+sudo apt-get install python-pip
+
 echo 'Get right version of nodejs, npm'
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get install -y nodejs
+
 sudo apt-get install -y npm
 
-echo 'Setting registry for npm'
+echo 'Setting registry'
 npm config set registry http://registry.npmjs.org/
 
 echo 'Installing express'
